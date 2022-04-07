8ac6370957eb7bd813f3209b17d7ca6d3e96fa97
sabyasachi94sen <64438737+sabyasachi94sen@users.noreply.github.com>
2022-04-07 18:53:55 +0530

    http-module

---
+++ index.js
@@ -0,0 +1,15 @@
+const http=require("http"); //es5 writing format
+//creating a server
+
+//Single API will run in defualt route url ie. /
+http.createServer((request,response)=>{
+
+    var user={
+        name:"sabya",
+        age:26,
+    }
+   response.write(JSON.stringify(user));
+
+   //closing the server
+   response.end();
+}).listen(3000);         
