51e1c77d28e6680526f089ab387a27cf9a8381d5
sabyasachi94sen <64438737+sabyasachi94sen@users.noreply.github.com>
2022-04-07 19:06:30 +0530

    Add files via upload
    http-module

---
+++ index.js
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+const http=require("http"); //es5 writing format
+//creating a server
+
+//Single API will run in defualt route url ie. /
+http.createServer((request,response)=>{
+
+    var user={
+        name:"sabya",
+        age:26,
+    }
+   response.write(JSON.stringify(user));
+
+   //closing the server
+   response.end();
+=======
+const http=require("http"); //es5 writing format
+//creating a server
+
+//Single API will run in defualt route url ie. /
+http.createServer((request,response)=>{
+
+    var user={
+        name:"sabya",
+        age:26,
+    }
+   response.write(JSON.stringify(user));
+
+   //closing the server
+   response.end();
+>>>>>>> 6f42ded (http-module)
+}).listen(3000);         
